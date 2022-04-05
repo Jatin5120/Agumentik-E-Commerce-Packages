256880e5d3a01fc4cc6d01d78ed737eb8b71c09a
Jatin <getjatin5120@gmail.com>
2022-04-05 21:26:13 +0530

    Added string constants

--- constants/lib/constants/string_constants.dart
+++ constants/lib/constants/string_constants.dart
@@ -23,7 +23,41 @@ class StorageKeys {
 class Strings {
   const Strings._();
 
-  static const String password = 'password';
+  static const String name = 'Name';
+  static const String firstName = 'First Name';
+  static const String middleName = "Middle Name";
+  static const String lastName = "Last Name";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String confirmPassword = "Confirm Password";
+  static const String phone = "Phone";
+  static const String phoneNumber = "Phone Number";
+  static const String dateOfBirth = "Date of Birth";
+  static const String dob = "DOB";
+  static const String dobFormat = "dd/MM/yyyy";
+  static const String gender = "Gender";
+  static const String enterDOB = 'Enter Date of Birth';
+
+  static const String submit = "Submit";
+  static const String save = "Save";
+  static const String continues = "Continue";
+  static const String login = "Login";
+  static const String signUp = "Sign Up";
+  static const String contactUs = "Contact Us";
+  static const String forgotPassword = "Forgot Password";
+  static const String resetPassword = "Reset Password";
+  static const String back = "Back";
+  static const String cancel = "Cancel";
+  static const String next = "Next";
+  static const String done = "Done";
+  static const String logout = "Logout";
+  static const String edit = "Edit";
+  static const String delete = "Delete";
+  static const String yes = "Yes";
+  static const String no = "No";
+  static const String choose = "Choose";
+  static const String add = "Add";
+  static const String remove = "Remove";
 }
 
 class FirebaseErrors {
