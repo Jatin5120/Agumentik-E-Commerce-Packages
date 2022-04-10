b90946d6988cf82f88dcf3712a0b3a2f3caa8626
Jatin <getjatin5120@gmail.com>
2022-04-10 19:34:35 +0530

    added package images

--- helpers/lib/src/dialog_helper.dart
+++ helpers/lib/src/dialog_helper.dart
@@ -206,7 +206,7 @@ class DialogHelper {
                 children: [
                   SizedBox(
                     height: 75,
-                    child: Image.asset(kIllustrationPath + 'problem.png', package: 'helpers'),
+                    child: Image.asset(_package + kIllustrationPath + 'problem.png'),
                   ),
                   const SizedBox(height: 20),
                   Text(
